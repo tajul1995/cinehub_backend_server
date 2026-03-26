a121d9f193381ea220c59ab96883f45bc9596e5c
tajul islam <tajulislam199595@gmail.com>
2026-03-26 17:57:51 +0600

    jwt-commit

--- src/config/env.ts
+++ src/config/env.ts
@@ -9,6 +9,13 @@ interface EnvConfig {
     DATABASE_URL:string,
     BETTER_AUTH_SECRET:string,
     BETTER_AUTH_URL:string,
+    ACCESS_TOKEN_SECRET:string,
+    REFRESH_TOKEN_SECRET:string,
+    ACCESS_TOKEN_EXPIRES_IN:string,
+    REFRESH_TOKEN_EXPIRES_IN:string,
+    BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN:string,
+   BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE:string
+
 
 }
 const loadEnv=():EnvConfig=>{
@@ -17,7 +24,13 @@ const loadEnv=():EnvConfig=>{
         "NODE_ENV",
         "DATABASE_URL",
         "BETTER_AUTH_SECRET",
-        "BETTER_AUTH_URL"
+        "BETTER_AUTH_URL",
+        "ACCESS_TOKEN_SECRET",
+        "REFRESH_TOKEN_SECRET",
+        "ACCESS_TOKEN_EXPIRES_IN",
+        "REFRESH_TOKEN_EXPIRES_IN",
+        "BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN",
+        "BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE"
       ]
        requirmentVariables.forEach((variable)=>{
         if(!process.env[variable]){
@@ -29,7 +42,13 @@ const loadEnv=():EnvConfig=>{
             NODE_ENV:process.env.NODE_ENV as string,
             DATABASE_URL:process.env.DATABASE_URL as string,
             BETTER_AUTH_SECRET:process.env.BETTER_AUTH_SECRET as string,
-            BETTER_AUTH_URL:process.env.BETTER_AUTH_URL as string
+            BETTER_AUTH_URL:process.env.BETTER_AUTH_URL as string,
+            ACCESS_TOKEN_SECRET:process.env.ACCESS_TOKEN_SECRET as string,
+            REFRESH_TOKEN_SECRET:process.env.REFRESH_TOKEN_SECRET as string,
+            ACCESS_TOKEN_EXPIRES_IN:process.env.ACCESS_TOKEN_EXPIRES_IN as string,
+            REFRESH_TOKEN_EXPIRES_IN:process.env.REFRESH_TOKEN_EXPIRES_IN as string,
+            BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN:process.env.BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN as string,
+            BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE:process.env.BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE as string,
         }
 }
 export const envVars=loadEnv()
